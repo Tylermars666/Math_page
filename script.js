62ee00b134547aab9d11e512680e0947076313c4
Omar Piedrahita <omar.piedrahitat@gmail.com>
2023-02-10 10:54:08 -0500

    Agregué id para titulos de invisibles y lista de materias

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ function mostrarSisas(){
     document.getElementById("contenedor_visible").style.display = "none";
     document.getElementById("contenedor_si").style.display = "block";
 
-    const typingText = document.getElementById("titulo_invisible_yes");
+    textoEscrito("titulo_invisible_yes");
+    textoEscrito("texto_invisible_yes");
+    
+
+}
+
+function mostrarNonas(){
+
+    document.getElementById("contenedor_visible").style.display = "none";
+    document.getElementById("contenedor_no").style.display = "block";
+
+}
+
+function textoEscrito(identificador){
+
+    const typingText = document.getElementById(identificador);
     const text = typingText.textContent;
     typingText.textContent = "";
 
@@ -14,15 +29,10 @@ function mostrarSisas(){
         i++;
     } else {
         clearInterval(typingInterval);
+
     }
     }, 50);
 
-
 }
 
-function mostrarNonas(){
-
-    document.getElementById("contenedor_visible").style.display = "none";
-    document.getElementById("contenedor_no").style.display = "block";
-
-}
+  
